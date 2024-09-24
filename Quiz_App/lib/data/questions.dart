52fc660d3d682755545e6227bd50aad1765399f1
Rohit Jagdale <rohitjagdale012@gmail.com>
2024-09-25 00:39:51 +0530

    first commit

---
+++ Quiz_App/lib/data/questions.dart
@@ -0,0 +1,60 @@
+import 'package:quiz_app/model/quiz_questions.dart';
+
+import '../model/quiz_questions.dart';
+
+const questions = [
+  QuizQuestion(
+    'If A is the brother of B, B is the sister of C, and C is the father of D, how is A related to D?',
+    [
+      'Uncle',
+      'Father',
+      'Brother',
+      'Cousin',
+    ],
+  ),
+  QuizQuestion(
+    'A clock shows the time as 3:30. What is the angle between the hour and minute hand?',
+    [
+      '75 degrees',
+      '90 degrees',
+      '105 degrees',
+      '120 degrees',
+    ],
+  ),
+  QuizQuestion(
+    'In a certain code language, if CAT is coded as 3120, what would DOG be coded as?',
+    [
+      '4157',
+      '4174',
+      '4167',
+      '4145',
+    ],
+  ),
+  QuizQuestion(
+    'If the day before yesterday was Thursday, what day will it be the day after tomorrow?',
+    [
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+    ],
+  ),
+  QuizQuestion(
+    'A man walks 20 meters north, then turns right and walks 30 meters, then turns left and walks 15 meters. How far is he from the starting point?',
+    [
+      '55 meters',
+      '35 meters',
+      '45 meters',
+      '50 meters',
+    ],
+  ),
+  QuizQuestion(
+    'Find the next number in the series: 2, 6, 12, 20, 30, ...',
+    [
+      '42',
+      '44',
+      '40',
+      '46',
+    ],
+  ),
+];
---
+++ Quiz_App/lib/quiz.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/pages/questions_screen.dart';
+import 'package:quiz_app/pages/results_screen.dart';
+import 'package:quiz_app/pages/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer);
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+        onRestart: restartQuiz,
+      );
+    }
+
+    // final screenWidget = activeScreen == 'start-screen'
+    //     ? StartScreen(switchScreen)
+    //     : const QuestionsScreen();
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.black,
+                Colors.black,
+              ],
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
